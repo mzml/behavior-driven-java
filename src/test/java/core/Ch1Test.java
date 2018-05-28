89e79a8e0530e48633b6eac7705fa343b5441413
Muzamil Satar <muzamil@mzml.io>
2018-05-28 13:33:02 -0600

    java is great

---
+++ src/test/java/core/Ch1Test.java
@@ -0,0 +1,14 @@
+package core;
+
+import static org.testng.Assert.*;
+
+public class Ch1Test {
+
+    @org.testng.annotations.BeforeMethod
+    public void setUp() {
+    }
+
+    @org.testng.annotations.AfterMethod
+    public void tearDown() {
+    }
+}
