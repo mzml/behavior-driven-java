efbd1f932b40d4bab1b5be481e51794cd3d59878
mzml <muzamil_satar@comcast.com>
2018-12-14 21:36:17 -0700

    got glue steps in place

---
+++ src/test/java/core/PyramidBuilderSteps.java
@@ -0,0 +1,20 @@
+package core;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+
+public class PyramidBuilderSteps {
+
+    @Given("I want a pyramid with height {int}")
+    public void i_want_a_pyramid_with_height(Integer height) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("it should create this pyramid in the console")
+    public void it_should_create_this_pyramid_in_the_console(String testPyramid) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+}
